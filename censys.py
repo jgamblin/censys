09384d40863f133598fccdd179434ce81ee9892e
Jerry Gamblin <jerry.gamblin@gmail.com>
2015-12-18 16:15:51 -0600

    Update
    Updated with some great fixes by @BurnItWithTruth

--- censys.py
+++ censys.py
@@ -1,12 +1,12 @@
-#!/usr/bin/env python
-#-*- coding: utf-8 -*-# Name:     nocommonssids.py
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Name:     censys.py
 # Purpose:  CynsysIO API Script
 # By:       Jerry Gamblin
-# Date:     07.11.15
-# Modified  07.11.15
-# Rev Level 0.8
+# Date:     18.12.15
+# Rev Level 0.9 (With some great fixes by @BurnItWithTruth)
 # -----------------------------------------------
-
+ 
 from termcolor import colored
 import argparse
 import json
@@ -16,65 +16,66 @@ import locale
 import os
 import sys
 import ast
-
-	
+ 
+   
 class Censys:
-
-	def __init__(self, ip):
-
+ 
+    def __init__(self, ip):
+ 
 		self.API_URL = "https://www.censys.io/api/v1"
 		self.UID = "Get From Censys.IO"
 		self.SECRET = "Get From Censys.IO"
 		self.ip = ip
-
-	def search(self):
-
-		pages = float('inf')
-		page = 1
-
-		while page <= pages:  
-
-			params = {'query' : self.ip, 'page' : page}
-			res = requests.post(self.API_URL + "/search/ipv4", json = params, auth = (self.UID, self.SECRET))
-			payload = res.json()
-			
-
-			for r in payload['results']:
-
-				ip = r["ip"]
-				proto = r["protocols"][0].replace(' ', '-')
-				
-		
-			
-				print '[%s] IP: %s - Protocols: %s' % (colored('*', 'red'), ip, proto)
-				
-				
-				if '80/http' in proto:
-					self.view(ip)
-
-			pages = payload['metadata']['pages']
-			page += 1
-
-	def view(self, server):
-
-		res = requests.get(self.API_URL + ("/view/ipv4/%s" % server), auth = (self.UID, self.SECRET))
-		payload = res.json()		
-
-		try:
-			if 'title' in payload['80']['http']['get'].keys():
-				print "[+] Title: %s" % payload['80']['http']['get']['title']
-			if 'server' in payload['80']['http']['get']['headers'].keys():
-				print "[+] Server: %s" % payload['80']['http']['get']['headers']['server']
-		except Exception as error:
-			print error
-		print ""
-
+ 
+    def search(self):
+ 
+        pages = float('inf')
+        page = 1
+ 
+        while page <= pages:  
+ 
+            params = {'query' : self.ip, 'page' : page}
+            res = requests.post(self.API_URL + "/search/ipv4", json = params, auth = (self.UID, self.SECRET))
+            payload = res.json()
+           
+ 
+            for r in payload['results']:
+               
+                ip = r["ip"]
+                proto = r["protocols"]
+                proto = [p.split("/")[0] for p in proto]
+                proto.sort(key=float)
+                protoList = ','.join(map(str, proto))          
+       
+           
+                print '[%s] IP: %s - Protocols: %s' % (colored('*', 'red'), ip, protoList)
+               
+               
+                if '80' in protoList:
+                    self.view(ip)
+ 
+            pages = payload['metadata']['pages']
+            page += 1
+ 
+    def view(self, server):
+ 
+        res = requests.get(self.API_URL + ("/view/ipv4/%s" % server), auth = (self.UID, self.SECRET))
+        payload = res.json()       
+ 
+        try:
+            if 'title' in payload['80']['http']['get'].keys():
+                print "[+] Title: %s" % payload['80']['http']['get']['title']
+            if 'server' in payload['80']['http']['get']['headers'].keys():
+                print "[+] Server: %s" % payload['80']['http']['get']['headers']['server']
+        except Exception as error:
+            print error
+ 
 parser = argparse.ArgumentParser(description = 'CENSYS.IO Web Server Search')
 parser.add_argument('-f', '--find', help='CENSYS Search', required = True)
-
-
+ 
+ 
 args = parser.parse_args()
 ip = args.find
-
+ 
 censys = Censys(ip)
 censys.search()
